dad6c14584c045490f8f8cd84daebeabc64149fa
Sander <65625476+sandermeis@users.noreply.github.com>
2022-12-14 10:45:46 +0100

    Formatting

--- RCWA v3.3.0/RCWA_transmittance.m
+++ RCWA v3.3.0/RCWA_transmittance.m
@@ -1,6 +1,8 @@
 function [Sz, fields, Kx, Ky, Kz] = RCWA_transmittance(layer, param, progressTick)
 
-Sz = zeros(param.num_H, ~param.calcAllRough*numel(layer)+param.calcAllRough*numel([layer.L])+2,numel(param.wavelengthArray));
+
+% Initialize results
+Sz = zeros(param.num_H, ~param.calcAllRough * numel(layer) + param.calcAllRough * numel([layer.L]) + 2, numel(param.wavelengthArray));
 Kx = zeros(param.num_H, numel(param.wavelengthArray));
 Ky = zeros(param.num_H, numel(param.wavelengthArray));
 Kz = cell(1, numel(param.wavelengthArray));
@@ -35,25 +37,25 @@ mu_trn = param.mu_trn(iWavelength);
 
 % Boundary conditions reflection region
 
-A1 = -1i/mu_ref*(param.Kz_ref\param.Kx)*param.Ky;
-A2 = -1i/mu_ref*(param.Kz_ref\(param.Ky*param.Ky+param.Kz_ref*param.Kz_ref));
-A3 = 1i/mu_ref*(param.Kz_ref\(param.Kx*param.Kx+param.Kz_ref*param.Kz_ref));
-A4 = 1i/mu_ref*(param.Kz_ref\param.Kx)*param.Ky;
+A1 = -1i / mu_ref * (param.Kz_ref \ param.Kx) * param.Ky;
+A2 = -1i / mu_ref * (param.Kz_ref \ (param.Ky * param.Ky + param.Kz_ref * param.Kz_ref));
+A3 = 1i / mu_ref * (param.Kz_ref \ (param.Kx * param.Kx + param.Kz_ref * param.Kz_ref));
+A4 = 1i / mu_ref * (param.Kz_ref \ param.Kx) * param.Ky;
 
-A=[eye(2*param.num_H);...
-    A1,A2;...
-    A3,A4];
+A = [eye(2 * param.num_H);...
+    A1, A2;...
+    A3, A4];
 
 % Boundary conditions transmission region
 
-B1 = 1i/mu_trn*(param.Kz_trn\param.Kx)*param.Ky;
-B2 = 1i/mu_trn*(param.Kz_trn\(param.Ky*param.Ky+param.Kz_trn*param.Kz_trn));
-B3 = -1i/mu_trn*(param.Kz_trn\(param.Kx*param.Kx+param.Kz_trn*param.Kz_trn));
-B4 = -1i/mu_trn*(param.Kz_trn\param.Kx)*param.Ky;
+B1 = 1i / mu_trn * (param.Kz_trn \ param.Kx) * param.Ky;
+B2 = 1i / mu_trn * (param.Kz_trn \ (param.Ky * param.Ky + param.Kz_trn * param.Kz_trn));
+B3 = -1i / mu_trn * (param.Kz_trn \ (param.Kx * param.Kx + param.Kz_trn * param.Kz_trn));
+B4 = -1i / mu_trn * (param.Kz_trn \ param.Kx) * param.Ky;
 
-B=[eye(2*param.num_H);...
-    B1,B2;...
-    B3,B4];
+B=[eye(2 * param.num_H);...
+    B1, B2;...
+    B3, B4];
 
 % Set equal athe the boundaries
 % s = incoming
@@ -79,10 +81,10 @@ B=[eye(2*param.num_H);...
 
 for i = numel(layer):-1:1
     % Loop backwards through sublayers
-    for j = size(layer(i).geometry.eps,3):-1:1
+    for j = size(layer(i).geometry.eps, 3):-1:1
         
         % Calculate V, W matrices and eigenvalue
-        [V, W, lam] = calc_VW(layer(i).geometry.mu,layer(i).geometry.eps(:,:,j), param.Kx, param.Ky);
+        [V, W, lam] = calc_VW(layer(i).geometry.mu,layer(i).geometry.eps(:, :, j), param.Kx, param.Ky);
         
         % Assemble field matrix
         F{i,j} = [W, W; -V, V];
@@ -91,53 +93,53 @@ for i = numel(layer):-1:1
         X{i,j} = expm(-lam * param.k_0(iWavelength) * layer(i).L(j));
 
         % Get a and b matrices, [a; b] = F^(-1) B
-        temp1 = F{i,j}\B;
+        temp1 = F{i,j} \ B;
 
-        a{i,j} = temp1(1:2*param.num_H,:);
-        b{i,j} = temp1(2*param.num_H+1:end,:);
+        a{i,j} = temp1(1:2 * param.num_H, :);
+        b{i,j} = temp1(2 * param.num_H + 1:end, :);
 
         % Effective 'B' of lower layer
-        B = F{i,j} * [eye(2*param.num_H),zeros(2*param.num_H);zeros(2*param.num_H),X{i,j}] * [eye(2*param.num_H);b{i,j}*(a{i,j}\X{i,j})];
+        B = F{i,j} * [eye(2*param.num_H), zeros(2*param.num_H); zeros(2*param.num_H), X{i, j}] * [eye(2*param.num_H); b{i, j} * (a{i, j} \ X{i, j})];
     end
 end
 
 % Now B is 'complete', solve for reflection region boundary conditions
 
-temp2 = [-A, B]\param.s_inc;
-r = temp2(1:2*param.num_H);
-tN{1} = temp2(2*param.num_H+1:end);
+temp2 = [-A, B] \ param.s_inc;
+r = temp2(1:2 * param.num_H);
+tN{1} = temp2(2 * param.num_H + 1:end);
 
 %% Looping forward: with t1, a and X, now all t are solved recursively %%
 
 k = 1;
 for i = 1:numel(layer)
-    for j = 1:size(layer(i).geometry.eps,3)
-        temp3 = a{i,j}\X{i,j};
-        tN{k+1} = temp3*tN{k};
-        c_im{i,j} = [eye(2*param.num_H);b{i,j}*temp3]*tN{k};
+    for j = 1:size(layer(i).geometry.eps, 3)
+        temp3 = a{i,j} \ X{i,j};
+        tN{k + 1} = temp3 * tN{k};
+        c_im{i, j} = [eye(2 * param.num_H); b{i, j} * temp3] * tN{k};
 
         % calcAllRough 1: All sublayer fields are calculated
         if param.calcAllRough
             % Calculate fields
-            field_begin = F{i,j}*[eye(2*param.num_H),zeros(2*param.num_H);zeros(2*param.num_H),X{i,j}]*c_im{i,j};
-            field_end = F{i,j}*[X{i,j},zeros(2*param.num_H);zeros(2*param.num_H),eye(2*param.num_H)]*c_im{i,j};
+            field_begin = F{i, j} * [eye(2 * param.num_H), zeros(2 * param.num_H); zeros(2 * param.num_H), X{i, j}] * c_im{i, j};
+            field_end = F{i, j} * [X{i, j}, zeros(2 * param.num_H); zeros(2 * param.num_H), eye(2 * param.num_H)] * c_im{i, j};
 
             % Calculate poynting vector
-            Sz(:,k) = (Sz_field(field_begin)-Sz_field(field_end))/sum(Sz_field(param.s_inc));
+            Sz(:, k) = (Sz_field(field_begin) - Sz_field(field_end)) / sum(Sz_field(param.s_inc));
         end
-        k=k+1;
+        k = k + 1;
 
     end
 
     % calcAllRough 0: Only first and last sublayer fields are calculated
     if ~param.calcAllRough
         % Calculate field at start of first layer
-        field_begin = F{i,1}*[eye(2*param.num_H),zeros(2*param.num_H);zeros(2*param.num_H),X{i,1}]*c_im{i,1};
+        field_begin = F{i, 1} * [eye(2 * param.num_H), zeros(2 * param.num_H); zeros(2 * param.num_H), X{i, 1}] *c _im{i, 1};
         % Calculate field at end of last layer
-        field_end = F{i,j}*[X{i,j},zeros(2*param.num_H);zeros(2*param.num_H),eye(2*param.num_H)]*c_im{i,j};
+        field_end = F{i, j} * [X{i,j}, zeros(2 * param.num_H); zeros(2 * param.num_H), eye(2 * param.num_H)] * c_im{i, j};
 
         % Calculate poynting vector
-        Sz(:,i) = (Sz_field(field_begin)-Sz_field(field_end))/sum(Sz_field(param.s_inc));
+        Sz(:, i) = (Sz_field(field_begin) - Sz_field(field_end)) / sum(Sz_field(param.s_inc));
     end
 
 end
@@ -145,24 +147,24 @@ end
 
 %% Diffraction efficiencies and return parameters %%
 
-t = temp3*tN{end-1};
+t = temp3 * tN{end - 1};
 
-r(2*param.num_H+1:3*param.num_H) = -param.Kz_ref\(param.Kx*r(1:param.num_H)+param.Ky*r(param.num_H+1:2*param.num_H));
-t(2*param.num_H+1:3*param.num_H) = -param.Kz_trn\(param.Kx*t(1:param.num_H)+param.Ky*t(param.num_H+1:2*param.num_H));
+r(2 * param.num_H + 1:3 * param.num_H) = - param.Kz_ref \ (param.Kx * r(1:param.num_H) + param.Ky * r(param.num_H + 1:2 * param.num_H));
+t(2 * param.num_H + 1:3 * param.num_H) = - param.Kz_trn \ (param.Kx * t(1:param.num_H) + param.Ky * t(param.num_H + 1:2 * param.num_H));
 
 % Reflection region
-rmag = abs(r(1:param.num_H)).^2+abs(r(param.num_H+1:2*param.num_H)).^2+abs(r(2*param.num_H+1:end)).^2;
-Sz(:,end+1) = real(param.Kz_ref*rmag/param.beta(3));
+rmag = abs(r(1:param.num_H)).^2 + abs(r(param.num_H + 1:2 * param.num_H)).^2 + abs(r(2 * param.num_H + 1:end)).^2;
+Sz(:, end + 1) = real(param.Kz_ref * rmag / param.beta(3));
 
 % Transmission region
-tmag = abs(t(1:param.num_H)).^2+abs(t(param.num_H+1:2*param.num_H)).^2+abs(t(2*param.num_H+1:end)).^2;
-Sz(:,end+1) = real(param.Kz_trn*tmag/param.beta(3));
+tmag = abs(t(1:param.num_H)).^2 + abs(t(param.num_H + 1:2 * param.num_H)).^2 + abs(t(2 * param.num_H + 1:end)).^2;
+Sz(:, end + 1) = real(param.Kz_trn * tmag / param.beta(3));
 
 % Return wave vectors
 Kx=diag(param.Kx);
 Ky=diag(param.Ky);
 
-for index=1:numel(layer)
+for index = 1:numel(layer)
     Kz{index} = layer(index).geometry.Kz;
 end
 
@@ -200,19 +202,19 @@ function PQ = calc_PQ(eps, mu, Kx, Ky)
 %%%| Ky mu^-1 Ky - eps|     - Ky mu^-1 Kx |
 
 if and(isscalar(eps),isscalar(mu))
-    PQ = 1/mu*[Kx*Ky, eps*mu*eye(size(Kx))-Kx*Kx;
-        Ky*Ky-eps*mu*eye(size(Kx)), -Ky*Kx];
+    PQ = 1 / mu * [Kx * Ky, eps * mu * eye(size(Kx)) - Kx * Kx;
+        Ky * Ky - eps * mu * eye(size(Kx)), -Ky * Kx];
 else
-    PQ = [Kx*inv(mu)*Ky, eps-Kx*inv(mu)*Kx;
-        Ky*inv(mu)*Ky-eps, -Ky*inv(mu)*Kx];
+    PQ = [Kx * inv(mu) * Ky, eps - Kx * inv(mu) * Kx;
+        Ky * inv(mu) * Ky - eps, - Ky * inv(mu) * Kx];
 end
 
 end
 
 
 function sz = Sz_field(f)
-F = reshape(f,[],4);
-sz = 0.5*real(F(:,1).*conj(1i*F(:,4))-F(:,2).*conj(1i*F(:,3)));
+F = reshape(f, [], 4);
+sz = 0.5 * real(F(:, 1) .* conj(1i * F(:, 4)) - F(:, 2) .* conj(1i * F(:, 3)));
 end
 
 
@@ -234,7 +236,7 @@ phase = exp(1i * param.k_0(iWavelength) * (param.beta(1) * x_grid + param.beta(2
 total_index = 1;
 
 for i = 1:numel(layer)
-    num_zdim = size(layer(i).geometry.eps,3);
+    num_zdim = size(layer(i).geometry.eps, 3);
     % Loop through sublayers
     for j = 1:num_zdim
 
@@ -245,8 +247,8 @@ for i = 1:numel(layer)
 
         for z = 1:numel(z_grid) - 1
             
-            f = F{i,j} * [diag(diag(X{i,j}).^(z_grid(z)/d)), zeros(2*param.num_H);...
-                        zeros(2*param.num_H), diag(diag(X{i,j}).^(-(z_grid(z)-d)/d))] * c_im{i,j};
+            f = F{i, j} * [diag(diag(X{i, j}).^(z_grid(z) / d)), zeros(2 * param.num_H);...
+                        zeros(2 * param.num_H), diag(diag(X{i, j}).^(-(z_grid(z) - d) / d))] * c_im{i, j};
 
             f2 = reshape(f,[],4);
             ezf = -1i * inv(layer(i).geometry.eps(:, :, j)) * (param.Kx * f2(:,4) - param.Ky * f2(:, 3));
@@ -268,6 +270,6 @@ for i = 1:numel(layer)
     
 end
 
-fields={Ex,Ey,Ez};%,Hx,Hy,Hz};
+fields = {Ex, Ey, Ez};%,Hx,Hy,Hz};
 
 end
--- RCWA v3.3.0/get_sinc.m
+++ RCWA v3.3.0/get_sinc.m
@@ -1,28 +1,48 @@
 function param = get_sinc(param, iter)
 
-n_inc = sqrt(param.eps_ref(iter)*param.mu_ref(iter));
+% Phi is angle from x vector
+% Theta is angle from surface normal
+
+
+% Incoming wavevector
+n_inc = sqrt(param.eps_ref(iter) * param.mu_ref(iter));
 param.beta(1) = n_inc * sin(param.theta) * cos(param.phi);
 param.beta(2) = n_inc * sin(param.theta) * sin(param.phi);
 param.beta(3) = n_inc * cos(param.theta);
 
+% Normal vector
 normal = [0, 0, -1];
 
+% Unit vector for TE polarization, perpendicular to plane of incidence
 TE_direction = cross(param.beta, normal) / norm(cross(param.beta, normal));
+
+% Check for NaN when which occurs when vector is parallel to normal
 nancheck = all(isnan(TE_direction));
 TE_direction(isnan(TE_direction)) = 0;
-a_TE = TE_direction + nancheck * [0,1,0];
+
+% When vector is parallel to normal, TE direction is defined as +y
+a_TE = TE_direction + nancheck * [0, 1, 0];
+
+% TM polarization is defined
 a_TM = cross(a_TE, param.beta) / norm(cross(a_TE, param.beta));
 
+% Electric field defined in surface coordinates
 p = param.pTE * a_TE + param.pTM * a_TM;
 
+% Force unity when pTE and pTM do not sum to 1
 p = p / norm(p);
 
-delta = zeros(param.num_H,1);
-delta(ceil(end/2)) = 1; %only central harmonic
-
+% Magnetic fields: Hx = i*inv(mu)*(kz*Ey-ky*Ez), Hy = i*inv(mu)*(kx*Ez-kz*Ex)
 h1 = 1i / param.mu_ref(iter) * (param.beta(3) * p(2) - param.beta(2) * p(3));
 h2 = 1i / param.mu_ref(iter) * (param.beta(1) * p(3) - param.beta(3) * p(1));
 
+
+% Electric field in Fourier space and set central harmonic to 1 which gives
+% a plane wave
+delta = zeros(param.num_H, 1);
+delta(ceil(end/2)) = 1;
+
+% Incoming wave as: [Ex Ey Hx Hy]
 param.s_inc = [delta * p(1); delta * p(2); delta * h1; delta * h2];
 end
 
